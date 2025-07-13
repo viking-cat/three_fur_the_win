5b6bebfb71bc94c2283261a578ac4843a1683f8a
Viking Cat <37673761+viking-cat@users.noreply.github.com>
2025-07-13 21:04:04 +0200

    Cleanup

--- scripts/game.gd
+++ scripts/game.gd
@@ -5,9 +5,8 @@ extends Node2D
 @onready var tilemap := $NewSquareOnTheBlock # adjust path if needed
 @onready var cat_black := preload("res://assets/cat_black.tscn")
 @onready var cat_white := preload("res://assets/cat_white.tscn")
-@onready var cats: Array[Node2D] = []
-@onready var p1Label := $"Camera2D/P1 Tittle"
-@onready var p2Label := $"Camera2D/P2 Tittle"
+# @onready var p1Label := $"Camera2D/P1 Tittle"
+# @onready var p2Label := $"Camera2D/P2 Tittle"
 @onready var p1Turn := true
 @onready var cellDataWrappers
 
@@ -36,36 +35,32 @@ func _unhandled_input(event):
 			# No data in dictionary at these cell coordinates means it is free
 			if tilemapCoords not in cellDataWrappers:
 
-				# Instance sprite2D of cat
+				# Instantiate cat sprite and store player for each cell
 				if p1Turn:
-					# cats.append(cat_black.instantiate())
 					cellDataWrappers[ tilemapCoords ] = {
 						"player": 1,
 						"artInstance" : cat_black.instantiate()
 					}
 					p1Turn = false
 				else:
-					#cats.append(cat_white.instantiate())
-					#cellDataWrappers[ tilemapCoords ] = {"player": 2}
 					cellDataWrappers[ tilemapCoords ] = {
 						"player": 2,
 						"artInstance" : cat_white.instantiate()
 					}
 					p1Turn = true
-				# print("----------------------------------")
-				# print("cats size ", cats.size())
-				# print("tile coords ", tilemap.map_to_local(tilemapCoords))
-				# print("cellDataWrappers ", cellDataWrappers )
-				# cats.get(cats.size() - 1).position = Vector2i(
+				
+				# Calculate position for cat sprite placement
 				cellDataWrappers[tilemapCoords]["artInstance"].position = Vector2i(
 					tilemap.position.x + tilemap.map_to_local(tilemapCoords).x,
 					tilemap.position.y + tilemap.map_to_local(tilemapCoords).y
 				)
-				# add_child(cats.get(cats.size() - 1))
+				
+				# Add graphics to scene
 				add_child(cellDataWrappers[tilemapCoords]["artInstance"])
 				checkWinner()
 		else:
 			print("Clicked on EMPTY tile at ", tilemapCoords)
 
 func checkWinner() :
+	
 	print("Check winner")
