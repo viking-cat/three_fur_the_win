5d0259caf23e68a62e7e22b055f60c6be65bae8e
Viking Cat <37673761+viking-cat@users.noreply.github.com>
2025-07-13 20:58:35 +0200

    wrapper around each playable cell

--- scripts/game.gd
+++ scripts/game.gd
@@ -9,58 +9,61 @@ extends Node2D
 @onready var p1Label := $"Camera2D/P1 Tittle"
 @onready var p2Label := $"Camera2D/P2 Tittle"
 @onready var p1Turn := true
-var squareMetaData
+@onready var cellDataWrappers
 
 
 # _func = built-in functions
 func _ready():
-	squareMetaData = {}
+	cellDataWrappers = {}
 	var cats = Array([],TYPE_OBJECT,"Scene",null)
 	print("TileMap node is: ", tilemap)
 
 func _unhandled_input(event):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 		# Debug input
-		print("square click")
+		# print("square click")
 		
 		# Get position & cell
-		# var world_pos = get_global_mouse_position()
-		# var tile_coords = tilemap.local_to_map(world_pos)
-		# var tile_data = tilemap.get_cell_tile_data(tile_coords)
-		
-		# get_viewport().get_mouse_position()
-		# var tile_coords := get_viewport().get_mouse_position()
 		var tilemapMousePosition = tilemap.get_local_mouse_position()
 		var tilemapCoords = tilemap.local_to_map(tilemapMousePosition)
-		var tile_data = tilemap.get_cell_tile_data(tilemapCoords)
-		# print ("view pos: ", viewMousPosition)
-		# print ("global pos: ", world_pos)
-		# print ("tilemap pos: ", tilemap.get_local_mouse_position())
+		
+		# Get cell data (no graphics = no data)
+		var realCellData = tilemap.get_cell_tile_data(tilemapCoords)
 
-		# Check if cell in tilemap contains anything
-		if tile_data:
-			print("Clicked on SQUARE tile at ", tilemapCoords)
+		# Check if cell contained data
+		if realCellData:
 			
-			# Instance sprite2D of cat
-			if p1Turn:
-				cats.append(cat_black.instantiate())
-				squareMetaData[ tilemapCoords ] = {"player": 1}
-				p1Turn = false
-			else:
-				cats.append(cat_white.instantiate())
-				squareMetaData[ tilemapCoords ] = {"player": 2}
-				p1Turn = true
-			print("----------------------------------")
-			print("cats size ", cats.size())
-			print("tile coords ", tilemap.map_to_local(tilemapCoords))
-			print("squareMetadata ", squareMetaData )
-			cats.get(cats.size() - 1).position = Vector2i(
-				tilemap.position.x + tilemap.map_to_local(tilemapCoords).x,
-				tilemap.position.y + tilemap.map_to_local(tilemapCoords).y
-			)
-			#cats.get(cats.size() - 1).position = tilemap.map_to_local(tilemapCoords)
-			add_child(cats.get(cats.size() - 1))
-			checkWinner()
+			# No data in dictionary at these cell coordinates means it is free
+			if tilemapCoords not in cellDataWrappers:
+
+				# Instance sprite2D of cat
+				if p1Turn:
+					# cats.append(cat_black.instantiate())
+					cellDataWrappers[ tilemapCoords ] = {
+						"player": 1,
+						"artInstance" : cat_black.instantiate()
+					}
+					p1Turn = false
+				else:
+					#cats.append(cat_white.instantiate())
+					#cellDataWrappers[ tilemapCoords ] = {"player": 2}
+					cellDataWrappers[ tilemapCoords ] = {
+						"player": 2,
+						"artInstance" : cat_white.instantiate()
+					}
+					p1Turn = true
+				# print("----------------------------------")
+				# print("cats size ", cats.size())
+				# print("tile coords ", tilemap.map_to_local(tilemapCoords))
+				# print("cellDataWrappers ", cellDataWrappers )
+				# cats.get(cats.size() - 1).position = Vector2i(
+				cellDataWrappers[tilemapCoords]["artInstance"].position = Vector2i(
+					tilemap.position.x + tilemap.map_to_local(tilemapCoords).x,
+					tilemap.position.y + tilemap.map_to_local(tilemapCoords).y
+				)
+				# add_child(cats.get(cats.size() - 1))
+				add_child(cellDataWrappers[tilemapCoords]["artInstance"])
+				checkWinner()
 		else:
 			print("Clicked on EMPTY tile at ", tilemapCoords)
 
